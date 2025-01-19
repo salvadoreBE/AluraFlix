d4d349128c7d64a3e4710c2e8cf0a34fe2de7703
Salvador <jesustvaxok@gmail.com>
2025-01-19 13:17:15 -0700

    Validando urls de Youtube

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -3,16 +3,26 @@ import { z } from "zod";
 export const formSchema = z.object({
     title: z.string().min(1, "El titulo es obligatorio").max(50, "El titulo no puede tener más de 50 caracteres"),
     category: z.enum(["frontend", "backend", "innovation"]),
-    video: z.string().url("El video debe ser una url valida").min(1, "El video es obligatorio"),
-    description: z.string().min(1, "La descripción es obligatoria"),
+    video: z.string()
+    .url("El video debe ser una URL válida")
+    .regex(
+        /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)[\w-]{11}$/,
+        "El video debe ser una URL válida de YouTube"
+    )
+    .min(1, "El video es obligatorio"),    description: z.string().min(1, "La descripción es obligatoria"),
     isDefault: z.boolean()
 })
 
 export const formPostSchema = z.object({
     title: z.string().min(1, "El titulo es obligatorio").max(50, "El titulo no puede tener más de 50 caracteres"),
     category: z.enum(["frontend", "backend", "innovation"]),
-    video: z.string().url("El video debe ser una url valida").min(1, "El video es obligatorio"),
-    description: z.string().min(1, "La descripción es obligatoria"),
+    video: z.string()
+    .url("El video debe ser una URL válida")
+    .regex(
+        /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)[\w-]{11}$/,
+        "El video debe ser una URL válida de YouTube"
+    )
+    .min(1, "El video es obligatorio"),    description: z.string().min(1, "La descripción es obligatoria"),
     isDefault: z.boolean()
 })
 
